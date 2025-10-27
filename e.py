0778dfd57964754f92c93463533c0d0a55db8cab
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:34:34 -0300

    sucessor e antecessor

--- e.py
+++ e.py
@@ -1,5 +1,7 @@
+# e. Faça um programa que leia um número inteiro e mostre o seu antecessor e seu sucessor.
 num = input("Digite um número: ")
 ant = int(num) - 1
 suc = int(num) + 1
+
 
 print(f"O antecessor de {num} é {ant} e o sucessor é {suc}.")
