a05bbfdf67027d8cc7de998002c1ee0cdcf4a6d9
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:31:48 -0300

    mensagem de boas-vindas
    Added a welcome message for the user input.

--- a.py
+++ a.py
@@ -1,4 +1,6 @@
+# a. Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas para ela:
 nome = input("Digite seu nome: ")
 nomec = nome.capitalize()
+
 
 print(f"Olá {nomec}, é um prazer te conhecer!")
