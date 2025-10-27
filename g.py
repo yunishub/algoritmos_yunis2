1ca3e611b2e690ddbe708f724df46aa926039d1c
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:35:49 -0300

    conversor de metros

--- g.py
+++ g.py
@@ -1,3 +1,4 @@
+# g. Desenvolva um programa que leia uma distância de metros e mostre os valores relativos em outras medidas.
 dist = input("Digite a distância em metros: ").strip().replace(',', '.')
 m = float(dist)
 
@@ -7,4 +8,5 @@ print(f"{m} hm = {m*100} hectômetros")
 print(f"{m} dam = {m*10} decâmetros")
 print(f"{m} m = {m/10} decímetros") 
 print(f"{m} m = {m/100} centímetros")    
+
 print(f"{m} m = {m/1000} milímetros")
