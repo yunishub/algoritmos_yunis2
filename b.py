0923b6a6db21de7a76ae485732ecd8e63111d1e8
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:33:37 -0300

    nome e salario

--- b.py
+++ b.py
@@ -1,4 +1,4 @@
-# Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+# b. Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
 nome = input("Digite o nome: ")
 nomec = nome.capitalize()
 salario = input("Digite o salário: ")
