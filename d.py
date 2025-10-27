27fa11205cf4c4083eb83749b4fc4702013648de
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:34:04 -0300

    media de duas notas
    Added comments and reformatted input for clarity.

--- d.py
+++ d.py
@@ -1,4 +1,6 @@
+# d. Faça um programa que leia as duas notas de um aluno em uma matéria e mostre na tela a sua média na disciplina.
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
+
 print(f"Média na disciplina: {media:.2f}")
