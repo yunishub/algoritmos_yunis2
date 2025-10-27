4237f743abfcb7f9966084c076fbcb2d839df5a6
yunis <yunis.barbar@escola.pr.gov.br>
2025-10-27 16:35:15 -0300

    dobro e terço de num real
    Added a print statement for the third part of the number.

--- f.py
+++ f.py
@@ -1,6 +1,8 @@
+# f. Crie um algoritmo que leia um número real e mostre na tela o seu dobro e a sua terça parte.
 num = float(input("Digite um número real: "))
 dobro = num * 2
 terco = num / 3
 
 print(f"O dobro de {num} é {dobro}")
+
 print(f"A terça parte de {num} é {terco}")
